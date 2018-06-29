a4562c6467e4d04b19d96dab8a43c2cf9b276c06
hudson21 <camihudsondiaz@ittepic.edu.mx>
2018-06-29 00:24:33 -0500

    Review features at ProductComponent

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -13,6 +13,14 @@ export class ProductComponent implements OnInit {
 
   product: any;
 
+  myReview ={
+    title: '',
+    description: '',
+    rating: 0
+  };
+
+  btnDisabled = false;
+
   constructor(
     private rest: RestApiService,
     private data: DataService,
@@ -29,6 +37,28 @@ export class ProductComponent implements OnInit {
             : this.router.navigate(['/']);
         }).catch(error => this.data.error(error['message']));
     });
+  }
+
+  async postReview(){
+    this.btnDisabled = true;
+    try{
+      const data = await this.rest.post(
+        'http://localhost:3800/api/review',
+        {
+          productId: this.product._id,
+          title: this.myReview.title,
+          description: this.myReview.description,
+          rating: this.myReview.rating
+        }
+      );
+      data['success']
+        ? this.data.success(data['message'])
+        : this.data.error(data['message']);
+
+    }catch(error){
+      this.data.error(error['message']);
+    }
+    this.btnDisabled = false;
   }
 
 }
