39b95ba0ffb43cb2da65a47da18c670a8e4e9c69
hudson21 <camihudsondiaz@ittepic.edu.mx>
2018-06-29 23:52:20 -0500

    Ya terminé el curso :)

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -24,15 +24,15 @@ export class CartComponent implements OnInit {
     private rest: RestApiService
   ) { }
 
-  trackByCartItems(index: number, item: any){
+  trackByCartItems(index: number, item: any) {
     return item._id;
   }
 
-  get cartItems(){
+  get cartItems() {
     return this.data.getCart();
   }
 
-  get cartTotal(){
+  get cartTotal() {
     let total = 0;
     this.cartItems.forEach((data, index) => {
       total += data['price'] * this.quantities[index];
@@ -40,85 +40,82 @@ export class CartComponent implements OnInit {
     return total;
   }
 
-  removeProduct(index, product){
+  removeProduct(index, product) {
     this.quantities.splice(index, 1);
     this.data.removeFromCart(product);
   }
 
   ngOnInit() {
-    this.cartItems.forEach(data =>{
+    this.cartItems.forEach(data => {
       this.quantities.push(1);
     });
     this.handler = StripeCheckout.configure({
       key: environment.stripeKey,
       image: 'assets/img/logo.png',
       locale: 'auto',
-      token: async stripeToken =>{
+      token: async stripeToken => {
         let products;
         products = [];
-        this.cartItems.forEach((d, index) =>{
+        this.cartItems.forEach((d, index) => {
           products.push({
             product: d['_id'],
-            quantity: this.quantities[index]
+            quantity: this.quantities[index],
           });
         });
 
-        try{
+        try {
           const data = await this.rest.post(
             'http://localhost:3800/api/payment',
             {
               totalPrice: this.cartTotal,
               products,
-              stripeToken
-            }
+              stripeToken,
+            },
           );
           data['success']
-            ? (this.data.clearCart(), this.data.success('Purchase Successful :)'))
+            ? (this.data.clearCart(), this.data.success('Purchase Successful.'))
             : this.data.error(data['message']);
-
-        }catch(error){
+        } catch (error) {
           this.data.error(error['message']);
         }
-      }
+      },
     });
   }
 
 
-  validate(){
-    if(!this.quantities.every(data => data > 0)){
+  validate() {
+    if (!this.quantities.every(data => data > 0)) {
       this.data.warning('Quantity cannot be less than one.');
-    }else if(!localStorage.getItem('token')){
-      this.router.navigate(['/login'])
-        .then(() =>{
-          this.data.warning('You need to login before making a purchase');
-        });
-    }else if(!this.data.user['address']){
-      this.router.navigate(['/profile/address'])
-        .then(() =>{
-          this.data.warning('You need to login before making a purchase')
-        });
-    }else{
-      this.data.message= '';
+    } else if (!localStorage.getItem('token')) {
+      this.router.navigate(['/login']).then(() => {
+        this.data.warning('You need to login before making a purchase.');
+      });
+    } else if (!this.data.user['address']) {
+      this.router.navigate(['/profile/address']).then(() => {
+        this.data.warning('You need to login before making a purchase.');
+      });
+    } else {
+      this.data.message = '';
       return true;
     }
   }
 
-  checkout(){
+  checkout() {
     this.btnDisabled = true;
-    try{
-      if(this.validate()){
+    try {
+      if (this.validate()) {
         this.handler.open({
           name: 'Amazono',
           description: 'Checkout Payment',
           amount: this.cartTotal * 100,
-          closed: () =>{
+          closed: () => {
             this.btnDisabled = false;
-          }
+          },
         });
-      }else{
-        this.btnDisabled= false;
+      } else {
+        this.btnDisabled = false;
       }
-    }catch(error){
+    } catch (error) {
       this.data.error(error);
     }
   }
